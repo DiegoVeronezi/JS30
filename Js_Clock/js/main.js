8d0b2a591f4aa7263dfb9df1b9cac4e14d9d2839
DiegoVeronezi <diegoveronezi70@gmail.com>
2018-01-05 16:43:37 -0200

    Finish Clock

--- Js_Clock/js/main.js
+++ Js_Clock/js/main.js
@@ -3,24 +3,34 @@ const secondHand = document.querySelector('.second-hand');
 const minsHand = document.querySelector('.min-hand');
 const hoursHand = document.querySelector('.hour-hand');
 
+let its = 0;
+
 function setDate(){
   const now = new Date();
 
   const seconds = now.getSeconds();
-  const secondsDegrees = ((seconds/ 60) * 360) + 90;
-  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
-
   const mins = now.getMinutes();
-  const minsDegrees = ((mins/ 60) * 360) + ((seconds/60) * 6) + 90;  
-  minsHand.style.transform = `rotate(${minsDegrees}deg)`;  
-
   const hours = now.getHours();
-  const hoursDegrees = ((hours/ 12) * 360) + ((mins/60) * 30) + 90;
+
+  const x = (seconds / 60) * 360;
+
+
+  if (x === 0) {
+    its += 360;
+  }
+
+  const offset = its + x;
+  const secondsDegrees = offset + 90;
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+  
+  const minsDegrees = ((mins / 60) * 360) + ((seconds / 60) * 6) + 90;  
+  minsHand.style.transform = `rotate(${minsDegrees}deg)`;  
+  
+  const hoursDegrees = ((hours / 12) * 360) + ((mins / 60) * 30) + 90;
   hoursHand.style.transform = `rotate(${hoursDegrees}deg)`;
-
-  console.log(hours, mins);
-
 }
 
 
-setInterval(setDate,1000);
+setInterval(setDate, 1000);
+
+setDate();
